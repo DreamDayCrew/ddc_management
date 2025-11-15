0be13877add06dad0d59469a2067ef273ab3c9a1
Kumudha <info@dreamdaycrew.com>
2025-11-15 11:49:46 +0000

    metro config added for expo

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,17 +1,7 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('@expo/metro-config');
+// metro.config.js
 
-/** @type {import('expo/metro-config').MetroConfig} */
+const { getDefaultConfig } = require('expo/metro-config');
+
 const config = getDefaultConfig(__dirname);
-
-// Add support for web
-config.resolver.assetExts.push('cjs');
-
-// Add support for crypto
-config.resolver.extraNodeModules = {
-  ...config.resolver.extraNodeModules,
-  crypto: require.resolve('crypto-browserify'),
-  stream: require.resolve('stream-browserify'),
-};
 
 module.exports = config;
