5eda0a251329177475e57027fc64026044a8500f
Kumudha <info@dreamdaycrew.com>
2025-12-03 09:29:06 +0000

    Rating feature completed

--- mobile/src/screens/MoreScreen.tsx
+++ mobile/src/screens/MoreScreen.tsx
@@ -146,7 +146,7 @@ export default function MoreScreen({ navigation }: any) {
 
       <View style={styles.footer}>
         <Text style={[styles.footerText, { color: colors.text }]}>Dream Day Crew</Text>
-        <Text style={[styles.footerSubtext, { color: colors.textSecondary }]}>Event Management System v1.0.10</Text>
+        <Text style={[styles.footerSubtext, { color: colors.textSecondary }]}>Event Management System v1.0.12</Text>
       </View>
     </ScrollView>
   );
